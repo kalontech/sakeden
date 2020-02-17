927ae924f6c635111ed1a855686371561f3f15cc
Andriy Tsaryov <andriy.tsaryov@gmail.com>
2020-02-17 11:28:53 +0200

    Fix modals

--- src/components/modals.tsx
+++ src/components/modals.tsx
@@ -9,20 +9,21 @@ import MenuPopup from "./menu-popup"
 
 const modalStyles = {
   content: {
-    alignItems: "center",
     backgroundColor: "transparent",
     border: "none",
     borderRadius: 0,
-    bottom: 0,
-    display: "flex",
-    justifyContent: "center",
-    left: 0,
+    bottom: "unset",
+    left: "unset",
+    maxHeight: "100vh",
     padding: 0,
-    right: 0,
-    top: 0,
+    right: "unset",
+    top: "unset",
   },
   overlay: {
+    alignItems: "center",
     backgroundColor: "rgba(0, 0, 0, .5)",
+    display: "flex",
+    justifyContent: "center",
     zIndex: 200,
   },
 }
@@ -39,23 +40,23 @@ const Modals: React.FC = () => {
     <>
       <Modal
         closeTimeoutMS={100}
+        contentLabel="Cart"
         isOpen={isCartVisible}
         onRequestClose={(): void => {
           setIsCartVisible(false)
         }}
         style={modalStyles}
-        contentLabel="Cart"
       >
         <CartPopup />
       </Modal>
       <Modal
         closeTimeoutMS={100}
+        contentLabel="Menu"
         isOpen={isMenuVisible}
         onRequestClose={(): void => {
           setIsMenuVisible(false)
         }}
         style={modalStyles}
-        contentLabel="Menu"
       >
         <MenuPopup />
       </Modal>

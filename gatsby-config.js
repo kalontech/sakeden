63776c6d8ec3da41072644aaf221f82eb5e57b90
Roman Tsegelskyi <roman.tsegelskyi@gmail.com>
2020-03-30 14:14:21 +0300

    Change name

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,7 +56,7 @@ module.exports = {
         background_color: "#663399",
         display: "minimal-ui",
         // icon: "src/images/gatsby-icon.png", // This path is relative to the root of the site.
-        name: "gatsby-starter-default",
+        name: "Sakeden",
         // eslint-disable-next-line @typescript-eslint/camelcase
         short_name: "starter",
         // eslint-disable-next-line @typescript-eslint/camelcase

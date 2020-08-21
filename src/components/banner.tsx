f53f4d57b7fba0ac7222767f7d22c6501cff62b0
Roman Tsegelskyi <roman.tsegelskyi@gmail.com>
2020-08-21 12:09:22 +0300

    WIP

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,10 +1,40 @@
-import React from "react"
+import moment from "moment"
+import React, { useEffect, useState } from "react"
 import { Box, Container, Flex, Text } from "theme-ui"
-
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface BannerProps {}
 
+const calculateTimeLeft = (now: moment.MomentInput) => {
+  const END_OF_SALE = moment(new Date("2020-09-18"))
+  const duration = moment.duration(END_OF_SALE.diff(now))
+  const days = duration.asDays()
+  duration.subtract(moment.duration(days, "days"))
+
+  //Get hours and subtract from duration
+  const hours = duration.hours()
+  duration.subtract(moment.duration(hours, "hours"))
+
+  //Get Minutes and subtract from duration
+  const minutes = duration.minutes()
+  duration.subtract(moment.duration(minutes, "minutes"))
+  //Get seconds
+  const seconds = duration.seconds()
+  return {
+    days: Math.floor(days),
+    hours: 24 + hours,
+    minutes: 60 + minutes,
+    seconds: 60 + seconds,
+  }
+}
+
 const Banner: React.FC<BannerProps> = () => {
+  const [timeLeft, setTimeLeft] = useState<any>({})
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft(new Date()))
+    }, 1000)
+    return () => clearTimeout(timer)
+  })
   return (
     <Box sx={{ bg: "black" }}>
       <Container>
@@ -16,7 +46,11 @@ const Banner: React.FC<BannerProps> = () => {
           }}
         >
           <Text sx={{ color: "white", textAlign: "center" }}>
-            Order HK$600 by 4pm and get FREE SAME DAY DELIVERY!
+            END OF SEASON is HERE! Use code KAMPAINOW to receive 30% off your
+            whole order! {timeLeft && Math.floor(timeLeft["days"])} days{" "}
+            {timeLeft && timeLeft["hours"]}:{timeLeft && timeLeft["minutes"]}:
+            {timeLeft && timeLeft["seconds"]} left <br />
+            Order HK$600 by 4pm for FREE SAME DAY DELIVERY!
           </Text>
         </Flex>
       </Container>
